ccd67629613cde0af394899f7d21d2abfd6396b8
Grae Drake <graemdrake@gmail.com>
2013-12-14 03:32:40 -0500

    Figuring out linear transitions

--- Problem_1/js/application.js
+++ Problem_1/js/application.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
    $(".btn-info").on("click", function() {
       $(".btn-info").trigger("blur");
+      $(".progress-bar").toggleClass("expandify");
       var startTime = Date.now();
       var limit = parseInt($("#limit").text(), 10);
       var result = euler(limit);
-      runTimer(0);
       var endTime = Date.now();
       var executionTime = endTime - startTime;
       console.log(result);
@@ -13,23 +13,4 @@ $(document).ready(function() {
       $(".time").text(executionTime + " miliseconds");
       event.preventDefault();
    });
-
-   // Timeout function to impose 1 minute limit
-   function runTimer(n) {
-      var t1 = Date.now();
-      var t2;
-      function go(n){
-         var timer = n;
-         setTimeout(function() {
-            timer += 1000;
-            $(".progress-bar").css({"width": (timer / 60) + "%"});
-            console.log(timer);
-            if(timer < 6000) {
-               t2 = Date.now();
-               go(timer);
-            }
-         }, 1000);
-      }
-      go(n);
-   }
 });
