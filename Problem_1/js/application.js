af0f8dcca283652b03dbbd62f2a2e7a1579f2db8
Grae Drake <graemdrake@gmail.com>
2013-12-15 23:30:43 -0500

    Failed to implement CSS animations

--- Problem_1/js/application.js
+++ Problem_1/js/application.js
@@ -1,16 +1,25 @@
 $(document).ready(function() {
-   $(".btn-info").on("click", function() {
-      $(".btn-info").trigger("blur");
+   
+   // Event handler for Run button
+   $("#run").on("click", function() {
+      $("#run").trigger("blur");
       $(".progress-bar").toggleClass("expandify");
       var startTime = Date.now();
       var limit = parseInt($("#limit").text(), 10);
       var result = euler(limit);
       var endTime = Date.now();
       var executionTime = endTime - startTime;
-      console.log(result);
-      console.log("Execution Time " + executionTime);
       $(".result").text(result);
       $(".time").text(executionTime + " miliseconds");
       event.preventDefault();
+   });
+
+   // Event handler for Reset button
+   $("#reset").on("click", function() {
+      $("#reset").trigger("blur");
+      $("#limit").text("1000");
+      $(".result").text("");
+      $(".time").text("");
+      $(".progress-bar").removeClass("expandify");
    });
 });
