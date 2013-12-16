ae4c56aaf2a06edaab37c1d4cf27578a5d26824c
Grae Drake <graemdrake@gmail.com>
2013-12-16 00:12:09 -0500

    More animation fail

--- Problem_1/js/application.js
+++ Problem_1/js/application.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
    
-   // Event handler for Run button
+   // Event handler for Run button.
    $("#run").on("click", function() {
       $("#run").trigger("blur");
-      $(".progress-bar").toggleClass("expandify");
+      $(".progress-bar").toggleClass("expandify collapsed");
       var startTime = Date.now();
       var limit = parseInt($("#limit").text(), 10);
       var result = euler(limit);
@@ -14,12 +14,20 @@ $(document).ready(function() {
       event.preventDefault();
    });
 
-   // Event handler for Reset button
+   // Event handler for Reset button.
    $("#reset").on("click", function() {
       $("#reset").trigger("blur");
       $("#limit").text("1000");
       $(".result").text("");
       $(".time").text("");
       $(".progress-bar").removeClass("expandify");
+   });
+
+   // Event handler for hitting enter while editing the limit.
+   $("#limit").on("keypress", function(e) {
+      var code = e.keyCode || e.which;
+      if(code == 13) { //Enter keycode
+         $("#run").trigger("focus");
+         }
    });
 });
